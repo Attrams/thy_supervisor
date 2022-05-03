b3e7b86d30b386c9a3309a853dec1a97c45984f1
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-05-03 17:06:01 +0000

    update the ThySupervisor module

--- lib/thy_supervisor.ex
+++ lib/thy_supervisor.ex
@@ -1,18 +1,76 @@
 defmodule ThySupervisor do
-  @moduledoc """
-  Documentation for `ThySupervisor`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  # API
+  def start_link(child_spec_list) do
+    GenServer.start_link(__MODULE__, [child_spec_list])
+  end
 
-  ## Examples
+  def start_child(supervisor, child_spec) do
+    GenServer.call(supervisor, {:start_child, child_spec})
+  end
 
-      iex> ThySupervisor.hello()
-      :world
+  def terminate_child(supervisor, pid) when is_pid(pid) do
+    GenServer.call(supervisor, {:terminate_child, pid})
+  end
 
-  """
-  def hello do
-    :world
+  # Callback Functions
+  def init([child_spec_list]) do
+    Process.flag(:trap_exit, true)
+    state = child_spec_list |> start_children() |> Enum.into(Map.new())
+    {:ok, state}
+  end
+
+  def handle_call({:start_child, child_spec}, _from, state) do
+    case start_child(child_spec) do
+      {:ok, pid} ->
+        new_state = state |> Map.put(pid, child_spec)
+        {:reply, {:ok, pid}, new_state}
+
+      :error ->
+        {:reply, {:error, "error starting child"}, state}
+    end
+  end
+
+  def handle_call({:terminate_child, pid}, _from, state) do
+    case terminate_child(pid) do
+      :ok ->
+        new_state = state |> Map.delete(pid)
+        {:reply, :ok, new_state}
+
+      :error ->
+        {:reply, {:error, "error terminating child"}, state}
+    end
+  end
+
+  # Private Functions
+  defp start_children([child_spec | rest]) do
+    case start_child(child_spec) do
+      {:ok, pid} ->
+        [{pid, child_spec} | start_children(rest)]
+
+      :error ->
+        :error
+    end
+  end
+
+  defp start_children([]) do
+    []
+  end
+
+  defp start_child({mod, fun, args}) do
+    case apply(mod, fun, args) do
+      pid when is_pid(pid) ->
+        Process.link(pid)
+        {:ok, pid}
+
+      _ ->
+        :error
+    end
+  end
+
+  defp terminate_child(pid) do
+    Process.exit(pid, :kill)
+    :ok
   end
 end
